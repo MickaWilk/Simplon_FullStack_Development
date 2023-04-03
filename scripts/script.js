843fed908b1e62137b7bac1b99b14b94d6685e61
Wilk Michael <micka.wilk@gmail.com>
2023-04-03 15:38:24 +0200

    page airbnb reproduite

--- scripts/script.js
+++ scripts/script.js
@@ -11,13 +11,26 @@ function notationEnEtoiles(note) {
 }
 
 function randomDate() {
-  // Générer une date aléatoire en 2023
-  const dateDebut = moment('2023-01-01');
-  const dateFin = moment('2023-12-31');
-  const randomTimestamp = dateDebut + Math.random() * (dateFin - dateDebut);
-  const randomDate = moment(randomTimestamp).format('DD/MM/YYYY');
-  return randomDate;
+  Date = "30/01/2023 - 15/02/2023"
+  return Date;
 }
+
+function randomDate() {
+  // Générer une date aléatoire en 2023
+  const dateDebut = new Date('2023-01-01');
+  const dateFin = new Date('2023-12-31');
+
+  // Générer deux dates aléatoires entre dateDebut et dateFin
+  const timestampDebut = dateDebut.getTime() + Math.random() * (dateFin.getTime() - dateDebut.getTime());
+  const timestampFin = timestampDebut + Math.random() * (dateFin.getTime() - timestampDebut);
+
+  // Convertir les timestamps en dates et les formater en string au format DD/MM/YYYY
+  const dateDebutStr = new Date(timestampDebut).toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+  const dateFinStr = new Date(timestampFin).toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+
+  return `${dateDebutStr} - ${dateFinStr}`;
+}
+
 
 
 const cartesDiv = document.getElementById("cartes");
@@ -27,7 +40,7 @@ for (let i = 0; i < 20; i++) {
   const nom = `Lieu ${i+1}`;
   const note = Math.floor(Math.random() * 5) + 1; // Générer une note entre 1 et 5
   const type = Math.random() > 0.5 ? "Particulier" : "Professionnel"; // Choisir aléatoirement entre Particulier et Professionnel
-  const dates = `03 - 30`
+  const dates = `${randomDate()}`
   const prix = Math.floor(Math.random() * 300) + 50; // Générer un prix entre 50 et 350
 
   const imgSrc = `https://picsum.photos/300/200?random=${i+1}`;
---
+++ scripts/searchbar.js
@@ -0,0 +1,10 @@
+// JavaScript pour la recherche en temps réel
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", performSearch);
+searchInput.addEventListener("keyup", performSearch);
+
+function performSearch() {
+  const searchText = searchInput.value;
+  // Insérer le code de recherche ici
+}
